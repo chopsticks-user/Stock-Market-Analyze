9b66da4707d20353812c0d107c998d118cf0b0f8
Frost <huuquangcap@gmail.com>
2022-06-22 17:45:44 -0400

    added <simulated_market.py>, <ragb.py> added function <stock> to prepare traning data to <analyze.py>

--- analyze.py
+++ analyze.py
@@ -6,17 +6,19 @@ import os
 import time
 from tools import *
 
-stocks_path = "Stock/Data/Stocks"
-etfs_path = "Stock/Data/ETFs"
-stocks_list = os.listdir(stocks_path)
-etfs_list = os.listdir(etfs_path)
+def stock(stock_index):
+    stocks_path = "Stock/Data/Stocks"
+    etfs_path = "Stock/Data/ETFs"
+    stocks_list = os.listdir(stocks_path)
+    etfs_list = os.listdir(etfs_path)
 
-data = pd.read_csv(stocks_path + "/" + stocks_list[500])
-data = data.dropna()
+    data = pd.read_csv(stocks_path + "/" + stocks_list[stock_index])
+    data = data.dropna()
 
-price_changes = change(np.asarray(data[["Close"]]))
-print(price_changes)
+    price_changes = change(np.asarray(data[["Close"]]))
+    print(price_changes)
 
-sns.distplot(price_changes)
-plt.show(block = True)
+    # sns.distplot(price_changes)
+    # plt.show(block = False)
+    return price_changes
 
---
+++ ragb.py
@@ -0,0 +1,13 @@
+import numpy as np
+import threading
+import time
+
+from analyze import stock
+from gbras import GoalBasedRiskAwareSeller
+from simulated_market import SimulatedMarket
+from tools import *
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+class RiskAwareGreedyBuyer(object):
+    def __init__(self):
+        pass
